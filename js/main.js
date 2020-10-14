077beb6438d6cd73e930ddf8cb64deb4c404e8d5
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-10-14 12:26:03 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
+// header import
+// hero import
+// header main part
+// intro
+// we create
+// numbers import
+// services import
+import { ServicesContentData } from './data/ServicesContent.js';
+import { renderServicesContent } from './components/services/renderServicesContent.js';
+// Our Latest Project
+// NEWS
+// HOW WE WORK + ANIMATIONS
 // Recent blogo posts
 import { datarecentblogposts } from './data/RecentBlogPosts/datarecentblogposts.js';
 import { renderRecentBlogPosts } from './components/recentblogposts/renderRecentBlogPosts.js';
-// header import
-// hero import
-// about import
+// contacts
+// footer
 
-renderRecentBlogPosts(datarecentblogposts);
 // header logic
 // hero logic
-// about logic
+// header main part
+// intro
+// we create
+// numbers import
+// renderServicesContent(ServicesContentData);
+// Our Latest Project
+// NEWS
+// HOW WE WORK + ANIMATIONS
+renderRecentBlogPosts(datarecentblogposts);
+// contacts
+// footer
