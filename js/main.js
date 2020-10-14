dacbbb73b01507514ee5a66d020744b5102c524e
Gabija <gabija.arm@gmail.com>
2020-10-14 11:33:21 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 // Recent blogo posts
-import { datarecentblogposts } from './data/datarecentblogposts.js';
-import { renderrecentblogoposts } from './components/renderrecentblogoposts.js';
+import { datarecentblogposts } from "./data/datarecentblogposts.js";
+import { renderrecentblogoposts } from "./components/renderrecentblogoposts.js";
 // header import
 // hero import
 // about import
+// contacts import
+import { contentContactsData } from "./data/contentContacts.js";
+import { renderContentContactsPosts } from "./components/contentContacts/contentContactsPosts.js";
 
 renderrecentblogoposts(datarecentblogposts);
 // header logic
 // hero logic
 // about logic
+// contacts logic
+renderContentContactsPosts(contentContactsData);
