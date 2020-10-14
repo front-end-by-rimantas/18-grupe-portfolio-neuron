50f51c6398ffc22ce868e6f56316109119bdb69d
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-10-14 12:19:42 +0300

    Created folders; Updated JS logic; created test;

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Recent blogo posts
-import { datarecentblogposts } from './data/datarecentblogposts.js';
-import { renderrecentblogoposts } from './components/renderrecentblogoposts.js';
+import { datarecentblogposts } from './data/RecentBlogPosts/datarecentblogposts.js';
+import { renderRecentBlogPosts } from './components/recentblogposts/renderRecentBlogPosts.js';
 // header import
 // hero import
 // about import
 
-renderrecentblogoposts(datarecentblogposts);
+renderRecentBlogPosts(datarecentblogposts);
 // header logic
 // hero logic
 // about logic
