aa5e56c20ab736f9a946aec89b2ead2257869710
Gabija <gabija.arm@gmail.com>
2020-10-15 11:33:00 +0300

    contacts-info

--- js/components/contacts/renderPostContacts.js
+++ js/components/contacts/renderPostContacts.js
@@ -4,8 +4,8 @@ function renderPostsContacts(data) {
   let HTML = "";
   const contactsDOM = document.querySelector(data.path);
   const contactsPostsCount = data.infoPosts.length;
-  const allContacts = `<div class='con-icon'><i class= '${data.mainIcon}'></i></div>
-  <div class='con-title'><h2>${data.title}</h2></div>`;
+  const allContacts = `<div class='contact-top'><div class='con-icon'><i class= 'main-icon ${data.mainIcon}'></i></div>
+  <div class='con-title'><h2>${data.title}</h2></div></div>`;
 
   for (let i = 0; i < contactsPostsCount; i++) {
     const contact = data.infoPosts[i];
