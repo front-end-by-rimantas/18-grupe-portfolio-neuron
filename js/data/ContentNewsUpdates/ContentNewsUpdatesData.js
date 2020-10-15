2071b876c3b4570e29a198c61ead40ee12fae263
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-10-15 12:19:05 +0300

    added img

---
+++ js/data/ContentNewsUpdates/ContentNewsUpdatesData.js
@@ -0,0 +1,28 @@
+const NewsUpdateData = {
+    selector: '#news_block',
+    imagePath: './img/ContentNewsUpdates/',
+    services: [
+        {
+            imagealt:'first image',
+            image: 'image1.png',
+        }
+        {
+            imagealt:'second image',
+            image: 'image2.png',
+        }
+        {
+            imagealt:'third image',
+            image: 'image3.png',
+        }
+        {
+            imagealt:'fourth image',
+            image: 'image4.png',
+        }
+        {
+            imagealt:'fifth image',
+            image: 'image5.png',
+        }
+    ]
+}
+
+export { NewsUpdateData }
