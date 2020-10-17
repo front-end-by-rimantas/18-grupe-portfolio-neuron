dc90161d1ed127dd38602c73e5626d3a0eaf5712
Kristina Piekutė <70894872+TINAZee@users.noreply.github.com>
2020-10-17 21:51:28 +0300

    How we work part in js

---
+++ js/data/howWeWorksData/howWeWorksData.js
@@ -0,0 +1,27 @@
+const howWeWorksData = {
+    selector: '#howweworks_block',
+    works:[
+        {
+            icon: 'flaticon-code',
+            title: 'Data Generated',
+            active: true
+        },
+        {
+            icon: 'flaticon-machine-learning-1',
+            title: 'Data Stored',
+            active: true
+        },
+        {
+            icon: 'flaticon-machine-learning',
+            title: 'Data Processing',
+            active: true
+        },
+        {
+            icon: 'flaticon-automaton',
+            title: 'Actionable Insights',
+            active: true
+        },
+    ]
+}
+
+export { howWeWorksData }
