3835c2649961637ea646dcbd49b774df40ff64aa
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-10-18 08:45:39 +0300

    fixed bug onscroll  responsive

--- js/components/menu/menuPosition.js
+++ js/components/menu/menuPosition.js
@@ -1,14 +1,13 @@
 function menuPosition() {
-    if (document.body.scrollTop > document.querySelector('.nav-menu-items').offsetTop || document.documentElement.scrollTop > document.querySelector('.nav-menu-items').offsetTop) {
-        if (window.innerWidth>900){
+    if (window.innerWidth>900 && (document.body.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70 || document.documentElement.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70)) {
+        
             document.getElementById("container-main").style.top = "0";
             document.getElementById("container-main").style.position = "fixed";
             document.getElementById("container-main").style.background = "linear-gradient(0deg, #08C291, #09c67f)";
-            
-            } else {
-            document.getElementById("container-main").style.position = ""
-            document.getElementById("container-main").style.background = ""
-        }
     }
-  }   
+            else {
+                document.getElementById("container-main").style.position = ""
+                document.getElementById("container-main").style.background = ""
+        }
+    }   
 export {menuPosition};
