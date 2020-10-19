a8c5d107b8dfb5f60be3354b910b53453f6eae0f
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-10-19 12:09:34 +0300

    LatestProject section added

--- js/components/ContentNewsUpdates/generateNewsLetter.js
+++ js/components/ContentNewsUpdates/generateNewsLetter.js
@@ -3,7 +3,7 @@ function generateNewsLetter(data) {
                         '<span id="newsletter-flaticon-mail" class="flaticon-mail"></span></div>'+  
                     '<div class="newsletter-text col-8 col-md-12">'+
                         '<h1 class="newsletter-title">Don’t Miss Our News<br>And Updates!</h1>'+
-                        '<input type="email" id="email" placeholder="Enter your Email">'+
+                        '<input type="email" id="formEmail" placeholder="Enter your Email">'+
                         '<button id="newsletter-flaticon-send" class="flaticon-send"></button></div>'
 
    const newsletterDOM = document.querySelector(data.selector1);
