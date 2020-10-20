a80d94abd30ab7d07a638f227f7eead521419a27
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-10-20 09:46:28 +0300

    OurLatestProjectImg Grid

--- js/components/ContentNewsUpdates/generateNewsLetter.js
+++ js/components/ContentNewsUpdates/generateNewsLetter.js
@@ -1,10 +1,12 @@
 function generateNewsLetter(data) {
-    let newsletter = '<div class="newsletter-icon col-4 col-md-12">'+
-                        '<span id="newsletter-flaticon-mail" class="flaticon-mail"></span></div>'+  
-                    '<div class="newsletter-text col-8 col-md-12">'+
-                        '<h1 class="newsletter-title">Don’t Miss Our News<br>And Updates!</h1>'+
-                        '<input type="email" id="formEmail" placeholder="Enter your Email">'+
-                        '<button id="newsletter-flaticon-send" class="flaticon-send"></button></div>'
+    let newsletter = `<div class="newsletter-icon col-4 col-md-12">
+                        <span id="newsletter-flaticon-mail" class="flaticon-mail"></span>
+                    </div> 
+                    <div class="newsletter-text col-8 col-md-12">
+                        <h1 class="newsletter-title">Don’t Miss Our News<br>And Updates!</h1>
+                        <input type="email" id="formEmail" placeholder="Enter your Email">
+                        <button id="newsletter-flaticon-send" class="flaticon-send"></button>
+                    </div>`
 
    const newsletterDOM = document.querySelector(data.selector1);
 
