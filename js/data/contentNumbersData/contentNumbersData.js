1448715e190c9f5b3f3e7a87749ab75da422e488
thomasvort <thomas.vort@live.com>
2020-10-21 11:49:31 +0300

    contentNumbers update

---
+++ js/data/contentNumbersData/contentNumbersData.js
@@ -0,0 +1,19 @@
+const numbersData = [
+    {
+        number: 6000,
+        description: 'Complete Project'
+    },
+    {
+        number: 400,
+        description: 'NLP Experts'
+    },
+    {
+        number: 3500,
+        description: 'Satisfied Clients'
+    },
+    {
+        number: 8000,
+        description: 'Industries Served'
+    }
+];
+export  { numbersData };
