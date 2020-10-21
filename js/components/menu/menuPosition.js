1258c45d5705d5420222dd53b8e17a49d7984f38
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-10-21 08:33:16 +0300

    added close element

--- js/components/menu/menuPosition.js
+++ js/components/menu/menuPosition.js
@@ -1,5 +1,6 @@
 function menuPosition() {
-    if (window.innerWidth>900 && (document.body.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70 || document.documentElement.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70)) {
+    if (window.innerWidth>900 && (document.body.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70 || document.documentElement.scrollTop > document.querySelector('.nav-menu-items').offsetTop + 70)) 
+    {
             document.getElementById("container-main").style.top = "0";
             document.getElementById("container-main").style.position = "fixed";
             document.getElementById("container-main").style.background = "linear-gradient(0deg, #08C291, #09c67f)";
--- js/components/menu/renderMenuSingleGenerate.js
+++ js/components/menu/renderMenuSingleGenerate.js
@@ -9,7 +9,7 @@ function renderMenuSingleGenerate(data,alldata) {
     let href= data.split(" ").join("").toLowerCase().concat('.html')
     if (href==='home.html') {href='index.html'}
     if (alldata.length!=0){
-        subhtml=`<li class="onemenu item-hide"><a href="./${href}">${data.toUpperCase()}</a>
+        subhtml=`<li class="onemenu ${data} item-hide"><a href="./${href}">${data.toUpperCase()}</a>
         <div class="expand">
            <i class="fa fa-angle-down" aria-hidden="true"></i>
         </div>
@@ -17,7 +17,10 @@ function renderMenuSingleGenerate(data,alldata) {
         for (let i = 0; i < alldata.length; i++) {
             subhtml+=renderSUBmenuSingleGenerate(data, alldata[i]);
         }
-        subhtml+=`</div></li>`;
+    const closeHtml = ` <div class="close-hide">
+    <i class="fa fa-times" aria-hidden="true"></i>
+    </div>`;
+        subhtml+=closeHtml + `</div></li>`;
     return subhtml;
     } else{
         console.log();
