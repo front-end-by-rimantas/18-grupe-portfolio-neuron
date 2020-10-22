d541c0be5138e737f865474f4f41d6b943f4a08c
akinis81 <66510705+akinis81@users.noreply.github.com>
2020-10-22 15:40:33 +0300

    structure

---
+++ homePageTwo/js/components/renderComputerVisionAi.js
@@ -0,0 +1,11 @@
+class RenderComputerVisionAi{
+constructor (params){
+    this.params = params;
+    this.render();
+}
+render(){
+    console.log(this.params);
+};
+
+}
+export {RenderComputerVisionAi}
---
+++ homePageTwo/js/main.js
@@ -0,0 +1,27 @@
+
+// <----------DATA ---------------->
+// Header top
+// header content + Partner slider!
+// object detection
+// pose estimation
+// image segmentation
+// computer vision AI
+import { datacomputervisionai } from '../js/data/datacomputervisionai.js'
+import { RenderComputerVisionAi } from '../js/components/renderComputerVisionAi.js'
+// testimonials - clients reviews
+// recent blogs and news
+// contacts
+// footer
+
+// <----------------LOGIC------------->
+// Header top
+// header content + Partner slider!
+// object detection
+// pose estimation
+// image segmentation
+// computer vision AI
+ new  RenderComputerVisionAi(datacomputervisionai);
+// testimonials - clients reviews
+// recent blogs and news
+// contacts
+// footer
