7e6e0eefc83acbc0e7958773829fbd42ef4a5016
Gabija <gabija.arm@gmail.com>
2020-10-23 10:12:07 +0300

    dataheaderTop

---
+++ js/data/headerTop/dataHeaderTop.js
@@ -0,0 +1,20 @@
+const headerTopData = {
+  selector: "",
+  htLeft: {
+    mail: {
+      favicon: "",
+      href: "",
+      text: "",
+    },
+    phone: {
+      favicon: "",
+      text: "",
+    },
+  },
+  htRight: [
+    "fa fa-facebook",
+    "fa fa-google-plus",
+    "fa fa-twitter",
+    "fa fa-linkedin",
+  ],
+};
