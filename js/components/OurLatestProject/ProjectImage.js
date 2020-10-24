f90d043a5af390c8ec79aeb87974b21dfb30a5ec
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-10-24 13:29:45 +0300

    Changes

--- js/components/OurLatestProject/ProjectImage.js
+++ js/components/OurLatestProject/ProjectImage.js
@@ -8,19 +8,25 @@ class ProjectImage {
     generateHED(){
         return `<div class="ourLatestPoject col-12">
                     <h2 class="LatestProjectTitle">Our Latest Project</h2>
-            </div>`
+                </div>`
     }
 
     generateHTML(){
         return `<div class="row latestProjectImg">
-                    <img class="singleImg" src="./img/OurLatestProject/image1.jpg" alt="image1">
+                    <div class="spacingImg">
+                        <img class="singleImg" src="./img/OurLatestProject/image1.jpg" alt="image1">
+                    </div> 
                 </div> 
                 <div class="row latestProjectImg">
-                    <img class="singleImg" src="./img/OurLatestProject/image2.jpg" alt="image2">
-                </div>
+                    <div class="spacingImg">
+                        <img class="singleImg" src="./img/OurLatestProject/image2.jpg" alt="image2">
+                    </div>
+                </div> 
                 <div class="row latestProjectImg">
-                    <img class="singleImg" src="./img/OurLatestProject/image3.jpg" alt="image3">
-                </div>`
+                    <div class="spacingImg">
+                        <img class="singleImg" src="./img/OurLatestProject/image3.jpg" alt="image3">
+                    </div>
+                </div> `
     } 
 }
 export { ProjectImage }
