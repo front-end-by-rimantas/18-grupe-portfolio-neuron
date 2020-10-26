57796784b1b4a0c31b56179b366209e6e18a8fbf
Gabija <gabija.arm@gmail.com>
2020-10-26 11:08:50 +0200

    testimonial data js

---
+++ homePageTwo/js/data/testimonials/testimonialsData.js
@@ -0,0 +1,51 @@
+const testimonialsData = {
+  path: ".testimonials",
+  imgPath: "../homePageTwo/img/testimonials/",
+  imgAlt: "Client Avatar",
+  title: "TESTIMONIALS",
+  subtitle: "Happy Clients Reviews",
+  clientPost: [
+    {
+      name: "Liza Hyden",
+      position: "Developer",
+      review:
+        "When an unknown printer took a galley of type and scram bledit to make a type specimen book. Lorem Ipsum has been the industry’s standard to dummy text ever since the 1500s, when an thish unknown printer took a galley of type and scram bled specimen book.",
+      rating: [],
+      active: true,
+    },
+    {
+      name: "Luise Henrikes",
+      position: "CEO & Founder",
+      review:
+        "When an unknown printer took a galley of type and scram bled it to make a type specimen book. Lorem Ipsum has been the industry’s standard to dummy text ever since the 1500s, when an thish unknown printer took a galley of type and scram bled specimen book.",
+      rating: [],
+      active: true,
+    },
+    {
+      name: "James Franklin",
+      position: "CTO & Co-Founder",
+      review:
+        "When an unknown printer took a galley of type and scram bled it to make a type specimen book. Lorem Ipsum has been the industry’s standard to dummy text ever since the 1500s, when an thish unknown printer took a galley of type and scram bled specimen book.",
+      rating: [],
+      active: true,
+    },
+    {
+      name: "Jara Lopez",
+      position: "Manager",
+      review:
+        "When an unknown printer took a galley of type and scram bled it to make a type specimen book. Lorem Ipsum has been the industry’s standard to dummy text ever since the 1500s, when an thish unknown printer took a galley of type and scram bled specimen book.",
+      rating: [],
+      active: false,
+    },
+    {
+      name: "James Henry",
+      position: "Designer",
+      review:
+        "When an unknown printer took a galley of type and scram bled it to make a type specimen book. Lorem Ipsum has been the industry’s standard to dummy text ever since the 1500s, when an thish unknown printer took a galley of type and scram bled specimen book.",
+      rating: [],
+      active: false,
+    },
+  ],
+};
+
+export { testimonialsData };
