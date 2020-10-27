5993e4aa6f7fab0a8682fdaf554b1f1121347729
sabinalebediene <70894973+sabinalebediene@users.noreply.github.com>
2020-10-27 15:56:45 +0200

    removed mistakes

--- js/components/OurLatestProject/ProjectImage.js
+++ js/components/OurLatestProject/ProjectImage.js
@@ -12,13 +12,13 @@ class ProjectImage {
     }
 
     generateHTML(){
-        return `<div class="row latestProjectImg col-4 col-lg-6 col-sm-12">
+        return `<div class="row latestProjectImg col-4 col-lg-6 col-md-12">
                         <img class="singleImg" src="./img/OurLatestProject/image1.jpg" alt="image1">
                 </div> 
-                <div class="row latestProjectImg col-4 col-lg-6 col-sm-12">
+                <div class="row latestProjectImg col-4 col-lg-6 col-md-12">
                         <img class="singleImg" src="./img/OurLatestProject/image2.jpg" alt="image2">
                 </div> 
-                <div class="row latestProjectImg col-4 col-lg-6 col-sm-12">
+                <div class="row latestProjectImg col-4 col-lg-6 col-md-12">
                         <img class="singleImg" src="./img/OurLatestProject/image3.jpg" alt="image3">
                 </div> `
     } 
